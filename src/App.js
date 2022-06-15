2c8e0343d8b477dfbf2aff07920e118117e69b59
mirchandani-mohnish <80059522+mirchandani-mohnish@users.noreply.github.com>
2022-06-15 19:28:57 +0530

    Update: Code Cleaning and Dependency update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import "./styles.css";
-import MainNav from "./components/MainNav";
-import CustomCarasaul from "./components/CustomCarasaul";
-import AboutSection from "./components/AboutSection";
-import ContactMe from "./components/ContactMe";
-import TestSVG from "./components/TestSVG";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import CubeComponent from "./components/CubeComponent";
-import ProfileSect from "./components/ProfileSect";
-import Grid from "@mui/material/Grid";
-import Blob from "./icons/Blob.svg";
-import CreepyEyes from "./components/CreepyEyes";
+import "./styles.css";
+
 export default function App() {
-  const CubeSize = {
-    ht: "200px",
-    wdt: "200px"
+  const cubeSize = {
+    wdt: "200px",
+    ht: "200px"
   };
   return (
     <div className="App">
-      <section className="MainCubeBack">
-        <section className="MainCube">
-          <CubeComponent Style={CubeSize} />
-        </section>
-      </section>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<CubeComponent Style={cubeSize} />}></Route>
+      </Routes>
     </div>
   );
 }
--- src/components/MainNav.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./ComponentStyle.css";
-
-const MainNav = function () {
-  return <div></div>;
-};
-
-export default MainNav;
